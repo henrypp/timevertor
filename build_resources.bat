bb12d341e5916af461ecde5fa3a87357ff57e14c
henrypp <support@henrypp.org>
2017-12-29 20:23:19 +0600

    fixes

--- build_resources.bat
+++ build_resources.bat
@@ -2,5 +2,7 @@
 
 php "..\builder\make_resource.php" ".\src\resource.hpp"
 php "..\builder\make_locale.php" "TimeVertor" "timevertor" ".\bin\i18n" ".\src\resource.hpp" ".\bin\timevertor.lng"
+copy /y ".\bin\timevertor.lng" ".\bin\32\timevertor.lng"
+copy /y ".\bin\timevertor.lng" ".\bin\64\timevertor.lng"
 
 pause
