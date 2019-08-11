cac0b23ad1681ea6a81d0c77020dd887b4387e6e
henrypp <support@henrypp.org>
2019-08-11 21:01:21 +0600

    ++version & ++year

--- src/app.hpp
+++ src/app.hpp
@@ -1,13 +1,13 @@
 // TimeVertor
-// Copyright (c) 2012-2018 Henry++
+// Copyright (c) 2012-2019 Henry++
 
 #ifndef __APP_H__
 #define __APP_H__
 
 #define APP_NAME L"TimeVertor"
 #define APP_NAME_SHORT L"timevertor"
-#define APP_VERSION L"1.3.1"
-#define APP_VERSION_RES 1,3,1,0
-#define APP_COPYRIGHT L"(c) 2012-2018 " _APP_AUTHOR L". All Rights Reversed."
+#define APP_VERSION L"1.3.2"
+#define APP_VERSION_RES 1,3,2,0
+#define APP_COPYRIGHT L"(c) 2012-2019 " _APP_AUTHOR L". All Rights Reversed."
 
 #endif // __APP_H__
