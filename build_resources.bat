d4beba8ef34015b4ddc107987e584a0ba1be7412
henrypp <support@henrypp.org>
2021-01-10 14:43:46 +0600

    upd resource builder

--- build_resources.bat
+++ build_resources.bat
@@ -1,7 +1,7 @@
 @echo off
 
-php "..\builder\make_resource.php" ".\src\resource.hpp"
-php "..\builder\make_locale.php" "TimeVertor" "timevertor" ".\bin\i18n" ".\src\resource.hpp" ".\src\resource.rc" ".\bin\timevertor.lng"
+php "..\builder\make_resource.php" ".\src\resource.h"
+php "..\builder\make_locale.php" "TimeVertor" "timevertor" ".\bin\i18n" ".\src\resource.h" ".\src\resource.rc" ".\bin\timevertor.lng"
 copy /y ".\bin\timevertor.lng" ".\bin\32\timevertor.lng"
 copy /y ".\bin\timevertor.lng" ".\bin\64\timevertor.lng"
 
