621d052fd6ddb323bd710959dd78858a1fcff872
henrypp <support@henrypp.org>
2021-11-07 00:02:29 +0600

    ++version

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd ..\builder
-call build timevertor 1.4.2
+call build timevertor 1.4.3
 
 pause
--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"TimeVertor"
 #define APP_NAME_SHORT L"timevertor"
-#define APP_VERSION L"1.4.2"
-#define APP_VERSION_RES 1,4,2,0
+#define APP_VERSION L"1.4.3"
+#define APP_VERSION_RES 1,4,3,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2012-2021 " APP_AUTHOR L". All Rights Reversed."
