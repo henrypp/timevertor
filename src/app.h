6e03532d2e5f9c9df48df4f337e7fa3bdfa9ac22
henrypp <support@henrypp.org>
2022-01-22 14:39:05 +0600

    ++version

--- src/app.h
+++ src/app.h
@@ -1,11 +1,11 @@
 // TimeVertor
-// Copyright (c) 2012-2021 Henry++
+// Copyright (c) 2012-2022 Henry++
 
 #pragma once
 
 #define APP_NAME L"TimeVertor"
 #define APP_NAME_SHORT L"timevertor"
-#define APP_VERSION L"1.4.3"
-#define APP_VERSION_RES 1,4,3,0
+#define APP_VERSION L"1.4.4"
+#define APP_VERSION_RES 1,4,4,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2012-2021 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2012-2022 " APP_AUTHOR L". All Rights Reversed."
